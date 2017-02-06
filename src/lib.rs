1d0e206dba5b6b081d3e7c7e02ca0546c771cd5d
Jack O'Connor <oconnor663@gmail.com>
2017-02-06 00:07:48 -0500

    first version of the docs/README

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,27 @@
+//! A library for awaiting and killing child processes from multiple threads.
+//!
+//! The
+//! [`std::process::Child`](https://doc.rust-lang.org/std/process/struct.Child.html)
+//! type in the standard library provides
+//! [`wait`](https://doc.rust-lang.org/std/process/struct.Child.html#method.wait)
+//! and
+//! [`kill`](https://doc.rust-lang.org/std/process/struct.Child.html#method.kill)
+//! methods that take `&mut self`, making it impossible to kill a child process
+//! while another thread is waiting on it. That design works around a race
+//! condition in Unix's `waitpid` function, where a PID might get reused as soon
+//! as the wait returns, so a signal sent around the same time could
+//! accidentally get delivered to the wrong process.
+//!
+//! However with the newer POSIX `waitid` function, we can wait on a child
+//! without freeing its PID for reuse. That makes it safe to send signals
+//! concurrently. Windows has actually always supported this, by preventing PID
+//! reuse while there are still open handles to a child process. This library
+//! wraps `std::process::Child` for concurrent use, backed by these APIs.
+//!
+//! - [Docs](https://docs.rs/shared_child)
+//! - [Crate](https://crates.io/crates/shared_child)
+//! - [Repo](https://github.com/oconnor663/shared_child.rs)
+
 extern crate libc;
 
 use std::io;
