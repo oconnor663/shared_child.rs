7622a49f92c52c8b3ab51588ca9125be043201bb
Jack O'Connor <oconnor663@gmail.com>
2017-04-15 14:44:14 -0400

    fix macOS
    Example error: https://travis-ci.org/oconnor663/shared_child.rs/jobs/222387295
    
    MacOS leaves the siginfo_t struct uninitialized in the case where we
    pass WNOHANG and the child is still running. Zeroing the struct
    ourselves will recover the correct behavior. This is corroborated by a
    comment in the Darwin kernel sources:
    
    /*
     * The state of the siginfo structure in this case
     * is undefined.  Some implementations bzero it, some
     * (like here) leave it untouched for efficiency.
     *
     * Thus the most portable check for "no matching pid with
     * WNOHANG" is to store a zero into si_pid before
     * invocation, then check for a non-zero value afterwards.
     */
    
    See https://github.com/opensource-apple/xnu/blob/8cf668b09f7c419fadf6081d1f1bdd6c5033e708/bsd/kern/kern_exit.c#L2039-L2055

--- src/unix.rs
+++ src/unix.rs
@@ -15,7 +15,7 @@ pub fn get_handle(child: &Child) -> Handle {
 pub fn wait_without_reaping(handle: Handle) -> io::Result<()> {
     loop {
         let ret = unsafe {
-            let mut siginfo = std::mem::uninitialized();
+            let mut siginfo = std::mem::zeroed();
             libc::waitid(libc::P_PID,
                          handle.0 as libc::id_t,
                          &mut siginfo,
@@ -36,7 +36,10 @@ pub fn wait_without_reaping(handle: Handle) -> io::Result<()> {
 pub fn try_wait_without_reaping(handle: Handle) -> io::Result<bool> {
     let mut siginfo: libc::siginfo_t;
     let ret = unsafe {
-        siginfo = std::mem::uninitialized();
+        // Darwin doesn't touch the siginfo_t struct if the child hasn't exited
+        // yet. It expects us to have zeroed it ahead of time. See:
+        // https://github.com/opensource-apple/xnu/blob/8cf668b09f7c419fadf6081d1f1bdd6c5033e708/bsd/kern/kern_exit.c#L2039-L2055
+        siginfo = std::mem::zeroed();
         libc::waitid(libc::P_PID,
                      handle.0 as libc::id_t,
                      &mut siginfo,
@@ -52,7 +55,8 @@ pub fn try_wait_without_reaping(handle: Handle) -> io::Result<bool> {
         // The child has not exited.
         Ok(false)
     } else {
-        // This should be impossible if we've called waitid correctly.
+        // This should be impossible if we called waitid correctly. But it will
+        // show up on macOS if we forgot to zero the siginfo_t above, for example.
         Err(io::Error::new(io::ErrorKind::Other,
                            format!("unexpected si_signo from waitid: {}", siginfo.si_signo)))
     }
