8e88277758713aeae826d77c75e787b817e04766
Jack O'Connor <oconnor663@gmail.com>
2017-04-15 15:47:49 -0400

    fix test_into_inner_after_wait on Windows

--- src/lib.rs
+++ src/lib.rs
@@ -318,10 +318,13 @@ mod tests {
         shared_child.kill().unwrap();
         shared_child.wait().unwrap();
         let mut child = shared_child.into_inner();
-        // The child has already been waited on, so kill should be an
-        // InvalidInput error.
+        // The child has already been waited on, so kill should be an error.
         let kill_err = child.kill().unwrap_err();
-        assert_eq!(std::io::ErrorKind::InvalidInput, kill_err.kind());
+        if cfg!(windows) {
+            assert_eq!(std::io::ErrorKind::PermissionDenied, kill_err.kind());
+        } else {
+            assert_eq!(std::io::ErrorKind::InvalidInput, kill_err.kind());
+        }
         // But wait should succeed.
         child.wait().unwrap();
     }
